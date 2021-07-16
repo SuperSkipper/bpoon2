f3683f7caf316e9653e16f6e1093fc9598bae5bd
SuperSkipper <benjp.1122@gmail.com>
2021-07-16 00:54:24 -0400

    Initial Commit

---
+++ src/components/about.js
@@ -0,0 +1,35 @@
+import './about.css'
+const home = props => {
+    return (
+        <div id="main-container">
+            <div id="container-left">
+                <div id="container-top-left">
+                    <div>
+                        <button>TEst</button>
+                        <h1>Hi there, I'm Ben</h1>
+                        <p>
+                        Nice to meet you! I’m a student at the Universty of Georgia studying computer science and business management with an emphasis on human resources. 
+                        Professionally, I am currently interning as an open technology solutions consultant at Credera. I also work as a real estate agent from time-to-time 
+                        as a supplement to my income. Outside of work and school, I can usually be found working on personal projects such as this website, learning new 
+                        recipies to cook, reading up on new technologies, at the gym, or playing video games. My interests also include music production, UI/UX design, 
+                        and poker. Lately, these interests have expanded into home (apartment in my case) automation as well as IoT.
+                        </p>
+                    </div>
+                </div>
+                <div id="container-bottom-left">
+                   
+                </div>
+            </div>
+            <div id="container-right">
+               
+            </div>
+        </div>
+        
+    )
+}
+
+home.propTypes = {
+
+}
+
+export default home
---
+++ src/components/home.js
@@ -0,0 +1,50 @@
+import './home.css'
+import logo from '../assets/images/Signature.png'
+import github from '../assets/images/github.png'
+import linkedin from '../assets/images/linkedin.png'
+import soundcloud from '../assets/images/soundcloud-black.png'
+const home = props => {
+    return (
+        <div id="main-container">
+            <div id="container-left">
+                <div id="container-top-left">
+                    <span>
+                        <img className='logo' src= {logo} alt = 'Ben Signature'></img>
+                    </span>
+                    <div id= "header">
+                        <h1>Hi there, I'm Ben.</h1>
+                        <h1>A pleasure to meet you.</h1>
+                    </div>
+                </div>
+                <div id="container-bottom-left">
+                    <ul>
+                        <li>
+                            <img src={github} alt='GitHub' className="social"></img>
+                        </li>
+                        <li>
+                            <img src={linkedin} alt='LinkedIn' className="social"></img>
+                        </li>
+                        <li>
+                            <img src={soundcloud} alt='SoundCloud' className="social"></img>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="container-right">
+                <ul>
+                    <li>About Me</li>
+                    <li>My Resume</li>
+                    <li>Personal Projects</li>
+                    <li>Contact Me</li>
+                </ul>
+            </div>
+        </div>
+        
+    )
+}
+
+home.propTypes = {
+
+}
+
+export default home
